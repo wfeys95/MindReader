5583478c8e8e3ff805204e7500d86c0f7bdce9d7
wfeys95 <11029952+wfeys95@users.noreply.github.com>
2025-02-14 12:38:06 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -60,7 +60,3 @@ document.getElementById('done_reading_dialog').addEventListener('close', () =>{
     document.getElementById('reader_progress').value = 0
     document.querySelector('.text_reader').innerHTML = 'Analyzing brainwaves...'
 })
-
-document.querySelector('.md-dialog').addEventListener('cancel', () =>{
-    document.querySelector('.md-dialog').show()
-})
